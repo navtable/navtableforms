69174b87d8de5277fc1af9774e9a7cc6f8e40632
Francisco Puga <fpuga@cartolab.es>
2010-12-28 18:09:42 +0000

    Added scripts to generate xml file from a csv

--- trunk/extNavTableForms/scripts/createjavaclasses/TemplatesDefinition.py
+++ trunk/extNavTableForms/scripts/createjavaclasses/TemplatesDefinition.py
@@ -62,13 +62,3 @@ class BindingTemplate(Template):
       classmodel     = ""
       objectmodel    = ""
       classvalidator = ""
-
-class XMLTemplate(Template):
-      "Structure to store all variables needed for XML file containing all project metadata."""
-      layeralias = ""
-      layername  = ""
-      layergeom  = ""
-      tablename  = ""
-      primarykey = ""
-      recordset  = ""
-
---
+++ trunk/extNavTableForms/scripts/createxml/XMLTemplate.py
@@ -0,0 +1,14 @@
+from Cheetah.Template import Template
+
+class XMLTemplate(Template):
+      "Structure to store all variables needed for XML file containing all project metadata."""
+      layeralias = ""
+      layername  = ""
+      layergeom  = ""
+      tablename  = ""
+      primarykey = ""
+      recordset  = ""
+      firstTag   = ""
+      layerTag   = ""
+      lastTag    = ""
+
