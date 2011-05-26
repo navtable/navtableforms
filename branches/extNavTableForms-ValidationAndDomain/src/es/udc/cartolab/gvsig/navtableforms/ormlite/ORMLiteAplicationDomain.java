515887ecf33e9255b57fa4bda2cc52ed30b56038
Andrés Maneiro <amaneiro@icarto.es>
2011-05-26 18:01:28 +0000

    AplicationDomain is now case insensitive

--- branches/extNavTableForms-ValidationAndDomain/src/es/udc/cartolab/gvsig/navtableforms/ormlite/ORMLiteAplicationDomain.java
+++ branches/extNavTableForms-ValidationAndDomain/src/es/udc/cartolab/gvsig/navtableforms/ormlite/ORMLiteAplicationDomain.java
@@ -16,22 +16,34 @@ public class ORMLiteAplicationDomain {
 	domainValues = new HashMap<String, DomainValues>();
     }
 
+    /**
+     * This method is case insensitive: whatever the name of the component it
+     * will uppercase it
+     */
     public void addDomainValues(String component, DomainValues values) {
-	domainValues.put(component, values);
+	domainValues.put(component.toUpperCase(), values);
     }
 
-    public void addRule(String component, ValidationRule rule) {
+    /**
+     * This method is case insensitive: whatever the name of the component it
+     * will uppercase it
+     */
+    public void addRule(String componentName, ValidationRule rule) {
 	if (rule != null) {
-	    domainRules.put(component, rule);
+	    domainRules.put(componentName.toUpperCase(), rule);
 	}
     }
 
-    public DomainValues getDomainValuesForComponent(String name) {
-	return domainValues.get(name);
+    /**
+     * This method is case insensitive: whatever the name of the component it
+     * will uppercase it and return the proper component
+     */
+    public DomainValues getDomainValuesForComponent(String componentName) {
+	return domainValues.get(componentName.toUpperCase());
     }
 
-    public ValidationRule getValidationRuleForComponent(String name) {
-	return domainRules.get(name);
+    public ValidationRule getValidationRuleForComponent(String componentName) {
+	return domainRules.get(componentName.toUpperCase());
     }
 
     public ValidationRule createRule(String id) {
