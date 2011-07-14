33013df4ee82aeefc5d42f0258bb6159fd8b3405
Andrés Maneiro <amaneiro@icarto.es>
2011-07-14 16:16:23 +0000

    Update example listeners

--- trunk/extNavTableForms/examples/extNavTableFormsExample/src/es/udc/cartolab/gvsig/navtableformsexample/Example1Form.java
+++ trunk/extNavTableForms/examples/extNavTableFormsExample/src/es/udc/cartolab/gvsig/navtableformsexample/Example1Form.java
@@ -1,9 +1,12 @@
 package es.udc.cartolab.gvsig.navtableformsexample;
 
-import java.awt.Component;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
 import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
 
 import org.apache.log4j.Logger;
 
@@ -15,52 +18,19 @@ import es.icarto.gvsig.navtableforms.AbstractForm;
 public class Example1Form extends AbstractForm {
 
     private FormPanel form;
+    private JCheckBox chb;
+    private JTextField cmp;
+    private ComponentEnablerListener componentEnablerListener;
 
     public Example1Form(FLyrVect layer) {
 	super(layer);
+	initWindow();
+    }
+
+    private void initWindow() {
 	viewInfo.setHeight(350);
 	viewInfo.setWidth(450);
 	viewInfo.setTitle("Example 1");
-    }
-
-    @Override
-    protected void fillSpecificValues() {
-	enableComponentIfCheckBoxIsSelected("hay_anali", "resultado");
-    }
-
-    protected void enableComponentIfCheckBoxIsSelected(String chbName,
-	    String cmpName) {
-	JCheckBox chb = (JCheckBox) getFormBody().getComponentByName(chbName);
-	Component cmp = getFormBody().getComponentByName(cmpName);
-
-	if (chb.isSelected()) {
-	    cmp.setEnabled(true);
-	} else {
-	    cmp.setEnabled(false);
-	}
-    }
-
-    protected void setListeners() {
-	super.setListeners();
-	JCheckBox hay_anali = (JCheckBox) getFormBody().getComponentByName(
-		"hay_anali");
-	hay_anali.setActionCommand("hay_anali");
-	hay_anali.addActionListener(this);
-    }
-
-    protected void removeListeners() {
-	JCheckBox hay_anali = (JCheckBox) getFormBody().getComponentByName(
-		"hay_anali");
-	hay_anali.removeActionListener(this);
-	super.removeListeners();
-    }
-
-    public void actionPerformed(ActionEvent e) {
-	super.actionPerformed(e);
-	String action = e.getActionCommand();
-	if (action.equals("hay_anali")) {
-	    enableComponentIfCheckBoxIsSelected("hay_anali", "resultado");
-	}
     }
 
     @Override
@@ -80,4 +50,48 @@ public class Example1Form extends AbstractForm {
     public Logger getLoggerName() {
 	return Logger.getLogger("Example1Form");
     }
+
+    @Override
+    protected void fillSpecificValues() {
+	enableComponentIfCheckBoxIsSelected("hay_anali", "resultado");
+    }
+
+    private void enableComponentIfCheckBoxIsSelected(String chbName,
+	    String cmpName) {
+
+	if (chb.isSelected()) {
+	    cmp.setEnabled(true);
+	} else {
+	    cmp.setEnabled(false);
+	}
+    }
+
+    @Override
+    protected void setListeners() {
+	super.setListeners();
+
+	HashMap<String, JComponent> widgets = getWidgetComponents();
+
+	cmp = (JTextField) widgets.get("RESULTADO");
+	chb = (JCheckBox) widgets.get("HAY_ANALI");
+
+	componentEnablerListener = new ComponentEnablerListener();
+	chb.addActionListener(componentEnablerListener);
+    }
+
+    @Override
+    protected void removeListeners() {
+	chb.removeActionListener(componentEnablerListener);
+	super.removeListeners();
+    }
+
+    public class ComponentEnablerListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	    enableComponentIfCheckBoxIsSelected("hay_anali", "resultado");
+	}
+
+    }
+
 }
