ac51e6b9260f6f05cdb468be1f305172f339fa7c
Andrés Maneiro <amaneiro@icarto.es>
2012-08-13 13:56:32 +0200

    TableController has now create & delete methods

--- src/es/icarto/gvsig/navtableforms/utils/TableController.java
+++ src/es/icarto/gvsig/navtableforms/utils/TableController.java
@@ -20,12 +20,24 @@ package es.icarto.gvsig.navtableforms.utils;
 import java.util.HashMap;
 import java.util.Set;
 
+import com.hardcode.gdbms.driver.exceptions.InitializeWriterException;
 import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
+import com.hardcode.gdbms.engine.values.Value;
+import com.iver.cit.gvsig.exceptions.expansionfile.ExpansionFileWriteException;
+import com.iver.cit.gvsig.exceptions.visitors.StartWriterVisitorException;
+import com.iver.cit.gvsig.exceptions.visitors.StopWriterVisitorException;
+import com.iver.cit.gvsig.fmap.core.DefaultRow;
+import com.iver.cit.gvsig.fmap.core.IRow;
+import com.iver.cit.gvsig.fmap.drivers.ITableDefinition;
+import com.iver.cit.gvsig.fmap.edition.EditionEvent;
 import com.iver.cit.gvsig.fmap.edition.IEditableSource;
+import com.iver.cit.gvsig.fmap.edition.IWriteable;
+import com.iver.cit.gvsig.fmap.edition.IWriter;
 import com.iver.cit.gvsig.fmap.layers.SelectableDataSource;
 
 import es.udc.cartolab.gvsig.navtable.AbstractNavTable;
 import es.udc.cartolab.gvsig.navtable.ToggleEditing;
+import es.udc.cartolab.gvsig.navtable.format.ValueFactoryNT;
 import es.udc.cartolab.gvsig.navtable.format.ValueFormatNT;
 
 /**
@@ -50,10 +62,35 @@ public class TableController {
 	this.valuesChanged = new HashMap<String, String>();
     }
 
+    public void create(boolean saveAfterCreating)
+	    throws ExpansionFileWriteException,
+	    ReadDriverException {
+	Value[] defaultValues = new Value[values.size()];
+	for (int i = 0; i < values.size(); i++) {
+	    defaultValues[i] = ValueFactoryNT.createNullValue();
+	}
+	create(saveAfterCreating, defaultValues);
+    }
+
+    public void create(boolean saveAfterCreating, Value[] defaultValues)
+	    throws ExpansionFileWriteException, ReadDriverException {
+	ToggleEditing te = new ToggleEditing();
+	if (!model.isEditing()) {
+	    te.startEditing(model);
+	}
+	if (model instanceof IWriteable) {
+	    IRow row = new DefaultRow(defaultValues);
+	    model.doAddRow(row, EditionEvent.ALPHANUMERIC);
+	}
+	if (saveAfterCreating) {
+	    te.stopEditing(model);
+	}
+    }
+
     public void read(long position) throws ReadDriverException {
 	SelectableDataSource sds = model.getRecordset();
-	if(position != AbstractNavTable.EMPTY_REGISTER) {
-	    clearAll();
+	clearAll();
+	if (position != AbstractNavTable.EMPTY_REGISTER) {
 	    for (int i = 0; i < sds.getFieldCount(); i++) {
 		String name = sds.getFieldName(i);
 		values.put(
@@ -66,7 +103,7 @@ public class TableController {
 	}
     }
 
-    public void save(long position) throws ReadDriverException {
+    public void update(long position) throws ReadDriverException {
 	ToggleEditing te = new ToggleEditing();
 	boolean wasEditing = model.isEditing();
 	if (!wasEditing) {
@@ -78,7 +115,24 @@ public class TableController {
 	if (!wasEditing) {
 	    te.stopEditing(model);
 	}
-	this.read((int) position);
+	read((int) position);
+    }
+
+    public void delete(long position) throws StopWriterVisitorException,
+    InitializeWriterException, StartWriterVisitorException,
+    ReadDriverException {
+
+	model.startEdition(EditionEvent.ALPHANUMERIC);
+
+	IWriteable w = (IWriteable) model;
+	IWriter writer = w.getWriter();
+
+	ITableDefinition tableDef = model.getTableDefinition();
+	writer.initialize(tableDef);
+
+	model.doRemoveRow((int) position, EditionEvent.ALPHANUMERIC);
+	model.stopEdition(writer, EditionEvent.ALPHANUMERIC);
+	clearAll();
     }
 
     public void clearAll() {
