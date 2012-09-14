c1e4ec918b2d3d494e628e5c19748c5f6a1b96dc
Andrés Maneiro <amaneiro@icarto.es>
2012-09-14 16:53:26 +0200

    JPopupMenu: disable create/update options when table is empty

--- src/es/icarto/gvsig/navtableforms/gui/tables/JTableContextualMenu.java
+++ src/es/icarto/gvsig/navtableforms/gui/tables/JTableContextualMenu.java
@@ -16,54 +16,77 @@ public class JTableContextualMenu implements MouseListener {
 
     private JTable table;
     private IForm form;
+    private JMenuItem openMenuItem;
+    private JMenuItem updateMenuItem;
+    private JMenuItem deleteMenuItem;
+    private JPopupMenu popupMenu;
 
+    /**
+     * When attaching this listener to your table, you should care if it fills
+     * the whole space of its viewport or on empty tables it won't show up.
+     * 
+     * So, for this to work on empty tables, you should make:
+     * yourTable.setFillsViewportHeight(true);
+     * 
+     * More info:
+     * http://docs.oracle.com/javase/6/docs/api/javax/swing/JTable.html
+     * #setFillsViewportHeight(boolean)
+     * http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4310721
+     */
     public JTableContextualMenu(IForm form) {
 	this.form = form;
+	initContextualMenu();
     }
 
     public void mouseClicked(MouseEvent e) {
+	// Make sure the JTable fills the parent viewport for this listener to
+	// work on empty tables (see comment on constructor):
+	//
+	// yourTable.setFillsViewportHeight(true);
+	//
 	table = (JTable) e.getComponent();
-	if (!JTableUtils.hasRows(table)
-		|| (table.getSelectedRow() == NO_ROW_SELECTED)) {
-	    return;
-	}
 	if (e.getClickCount() == 2) {
 	    form.actionUpdateRecord(table.getSelectedRow());
 	} else if (e.getButton() == BUTTON_RIGHT) {
-	    openContextualMenu(e);
+	    if (!JTableUtils.hasRows(table)
+		    || (table.getSelectedRow() == NO_ROW_SELECTED)) {
+		updateMenuItem.setEnabled(false);
+		deleteMenuItem.setEnabled(false);
+	    } else {
+		updateMenuItem.setEnabled(true);
+		deleteMenuItem.setEnabled(true);
+	    }
+	    popupMenu.show(e.getComponent(), e.getX(), e.getY());
 	}
     }
 
-    private void openContextualMenu(MouseEvent e) {
-	JPopupMenu popup = new JPopupMenu();
+    private void initContextualMenu() {
+	popupMenu = new JPopupMenu();
 
-	// Create new record
-	JMenuItem open = new JMenuItem("Crear nuevo");
-	open.addActionListener(new ActionListener() {
+	// Create record
+	openMenuItem = new JMenuItem("Crear nuevo");
+	openMenuItem.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent arg0) {
 		form.actionCreateRecord();
 	    }
 	});
-	popup.add(open);
+	popupMenu.add(openMenuItem);
 
-	// Update record
-	JMenuItem update = new JMenuItem("Actualizar registro");
-	update.addActionListener(new ActionListener() {
+	updateMenuItem = new JMenuItem("Actualizar registro");
+	updateMenuItem.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent arg0) {
 		form.actionUpdateRecord(table.getSelectedRow());
 	    }
 	});
-	popup.add(update);
+	popupMenu.add(updateMenuItem);
 
-	// Delete record
-	JMenuItem delete = new JMenuItem("Borrar registro");
-	delete.addActionListener(new ActionListener() {
+	deleteMenuItem = new JMenuItem("Borrar registro");
+	deleteMenuItem.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent arg0) {
 		form.actionDeleteRecord(table.getSelectedRow());
 	    }
 	});
-	popup.add(delete);
-	popup.show(e.getComponent(), e.getX(), e.getY());
+	popupMenu.add(deleteMenuItem);
     }
 
     public void mouseEntered(MouseEvent arg0) {
