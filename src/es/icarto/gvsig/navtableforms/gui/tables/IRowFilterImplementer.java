39ee5119cebae0faad10d250f755800a3f4d9f39
Francisco Puga <fpuga@cartolab.es>
2013-03-12 17:19:11 +0100

    Avoid creating an instance of ValueFormat each time the method is called

--- src/es/icarto/gvsig/navtableforms/gui/tables/IRowFilterImplementer.java
+++ src/es/icarto/gvsig/navtableforms/gui/tables/IRowFilterImplementer.java
@@ -7,17 +7,19 @@ import es.udc.cartolab.gvsig.navtable.format.ValueFormatNT;
 
 public class IRowFilterImplementer implements IRowFilter {
 
+    private ValueFormatNT valueFormatNT;
     private int fieldIndex;
     private String fieldValue;
 
     public IRowFilterImplementer(int fieldIndex, String fieldValue) {
 	this.fieldIndex = fieldIndex;
 	this.fieldValue = fieldValue;
+	this.valueFormatNT = new ValueFormatNT();
     }
 
     public boolean evaluate(IRow row) {
 	Value value = row.getAttribute(fieldIndex);
-	if (value.getStringValue(new ValueFormatNT()).equals(fieldValue)) {
+	if (value.getStringValue(valueFormatNT).equals(fieldValue)) {
 	    return true;
 	}
 	return false;
