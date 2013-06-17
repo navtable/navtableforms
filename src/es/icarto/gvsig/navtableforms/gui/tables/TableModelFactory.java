df120eec62516c25f7dacede787c0f957ee2c995
Jorge López <jlopez@cartolab.es>
2013-06-17 14:40:17 +0200

    Table model with OR filter creator.
    Now the table model factory can create a model
    with multiple filters wrapped in an OR one.

--- src/es/icarto/gvsig/navtableforms/gui/tables/TableModelFactory.java
+++ src/es/icarto/gvsig/navtableforms/gui/tables/TableModelFactory.java
@@ -1,5 +1,6 @@
 package es.icarto.gvsig.navtableforms.gui.tables;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
@@ -24,6 +25,26 @@ public class TableModelFactory {
 	IRowFilter filter = new IRowFilterImplementer(
 		fieldIndex, rowFilterValue);
 	return new TableModelAlphanumeric(model, filter, columnNames, columnAliases);
+    }
+
+    public static TableModelAlphanumeric createFromTableWithOrFilter(
+	    String sourceTable,
+	    String rowFilterName, String[] rowFilterValues,
+	    String[] columnNames, String[] columnAliases)
+	    throws ReadDriverException {
+
+	TOCTableManager toc = new TOCTableManager();
+	IEditableSource model = toc.getTableByName(sourceTable).getModel()
+		.getModelo();
+	int fieldIndex = model.getRecordset()
+		.getFieldIndexByName(rowFilterName);
+	List<IRowFilter> filters = new ArrayList<IRowFilter>();
+	for (String rowFilterValue : rowFilterValues) {
+	    filters.add(new IRowFilterImplementer(fieldIndex, rowFilterValue));
+	}
+	return new TableModelAlphanumeric(model,
+		new IRowMultipleOrFilterImplementer(filters), columnNames,
+		columnAliases);
     }
 
 }
