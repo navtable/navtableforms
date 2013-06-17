cb6cb52ee680963bc92d12865a97be1134cb3386
Jorge López <jlopez@cartolab.es>
2013-06-17 14:39:21 +0200

    Added multiple filters Or wrapper.
    A filter for applying multiple simple filters
    with an OR logic has been added.

---
+++ src/es/icarto/gvsig/navtableforms/gui/tables/IRowMultipleOrFilterImplementer.java
@@ -0,0 +1,24 @@
+package es.icarto.gvsig.navtableforms.gui.tables;
+
+import java.util.List;
+
+import com.iver.cit.gvsig.fmap.core.IRow;
+
+public class IRowMultipleOrFilterImplementer implements IRowFilter {
+
+    private List<IRowFilter> filters;
+
+    public IRowMultipleOrFilterImplementer(List<IRowFilter> filters) {
+	this.filters = filters;
+    }
+
+    public boolean evaluate(IRow row) {
+	for (IRowFilter filter : filters) {
+	    if (filter.evaluate(row)) {
+		return true;
+	    }
+	}
+	return false;
+    }
+
+}
