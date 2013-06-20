b5f1b6f499ffd549c99408d1c7a743cd6dc89c32
Jorge López <jlopez@cartolab.es>
2013-06-20 14:18:47 +0200

    Dependent combobox handler also focuslistener.
    Now the handler works with focus events as well,
    which make more sense with textfields.
    Also refactored it to a better name.

--- src/es/icarto/gvsig/navtableforms/ormlite/domainvalidator/listeners/DependentComboboxHandler.java
+++ src/es/icarto/gvsig/navtableforms/ormlite/domainvalidator/listeners/DependentComboboxHandler.java
@@ -2,6 +2,8 @@ package es.icarto.gvsig.navtableforms.ormlite.domainvalidator.listeners;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.util.ArrayList;
 
 import javax.swing.JComboBox;
@@ -11,13 +13,26 @@ import javax.swing.JTextField;
 import es.icarto.gvsig.navtableforms.IValidatableForm;
 import es.icarto.gvsig.navtableforms.ormlite.domainvalues.KeyValue;
 
-public class DependentComboboxesHandler implements ActionListener {
+/**
+ * Handler for filling a dependent ComboBox
+ * 
+ * The Handler right now works with two different parent components: JTextfield
+ * and JComboBox. It implements two different interfaces with two different
+ * types of events: Action and Focus. Action is fired when you click onto a
+ * combo or press enter while inside a textfield, whereas focus is activated
+ * when one of those components loses it. You can assign the handler as the
+ * listener type you prefer as both events trigger the same code, though we
+ * suggest using the focus one on text fields, and the action one with combos.
+ */
+
+public class DependentComboboxHandler implements ActionListener,
+	FocusListener {
 
     private ArrayList<JComponent> parentComponents;
     private JComboBox comboBoxToFill;
     private IValidatableForm form;
 
-    public DependentComboboxesHandler(IValidatableForm form,
+    public DependentComboboxHandler(IValidatableForm form,
 	    JComponent parentComponent, JComboBox comboBoxToFill) {
 	this.form = form;
 	this.comboBoxToFill = comboBoxToFill;
@@ -25,7 +40,7 @@ public class DependentComboboxesHandler implements ActionListener {
 	this.parentComponents.add(parentComponent);
     }
 
-    public DependentComboboxesHandler(IValidatableForm form,
+    public DependentComboboxHandler(IValidatableForm form,
 	    ArrayList<JComponent> parentComponents, JComboBox comboBoxToFill) {
 	this.form = form;
 	this.parentComponents = parentComponents;
@@ -34,6 +49,22 @@ public class DependentComboboxesHandler implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+	// Value may have changed in a ComboBox,
+	// or enter is pressed inside a TextField.
+	if (!form.isFillingValues() && parentComponentsHaveItemSelected()) {
+	    updateComboBoxValues();
+	}
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+	// This event is of no use to us.
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+	// Value may have changed in a TextField or
+	// a ComboBox
 	if (!form.isFillingValues() && parentComponentsHaveItemSelected()) {
 	    updateComboBoxValues();
 	}
