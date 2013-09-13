c877e3acd1c1ab5eea59ffff179251175215f6db
Jorge López <jlopez@cartolab.es>
2013-09-13 19:04:43 +0200

    Make double rule work w/ current decimal separator
    Had to do a pretty ugly workaround in order to
    obtain the current decimal separator, but this
    was needed due to the current behaviour of
    NavTable saving.

--- src/es/icarto/gvsig/navtableforms/ormlite/domainvalidator/rules/DoublePositiveRule.java
+++ src/es/icarto/gvsig/navtableforms/ormlite/domainvalidator/rules/DoublePositiveRule.java
@@ -16,8 +16,19 @@
  */
 package es.icarto.gvsig.navtableforms.ormlite.domainvalidator.rules;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+import es.udc.cartolab.gvsig.navtable.format.DoubleFormatNT;
+
 
 public class DoublePositiveRule extends ValidationRule {
+
+    private NumberFormat format;
+
+    public DoublePositiveRule() {
+	format = DoubleFormatNT.getDisplayingFormat();
+    }
 
     @Override
     public boolean validate(String value) {
@@ -27,24 +38,25 @@ public class DoublePositiveRule extends ValidationRule {
     private boolean isDoublePositive(String value) {
 	try {
 	    value = removeStartingTrailingZeros(value);
-	    Double doubleValue = Double.parseDouble(value);
+	    Double doubleValue = format.parse(value).doubleValue();
 	    if ((doubleValue.toString().length() == value.length())
 		    && (doubleValue >= 0.0)) {
 		return true;
 	    }
 	    return false;
-	} catch (NumberFormatException nfe) {
+	} catch (ParseException nfe) {
 	    return false;
 	}
     }
 
     private String removeStartingTrailingZeros(String number) {
+	char decimalSeparator = format.format(1.1).charAt(1);
 	String aux = number.replaceAll("^[0]*", "").replaceAll("[0]*$", "")
-		.replaceAll("\\.$", "");
-	if (!aux.contains(".")) {
-	    aux += ".0";
+		.replaceAll("\\" + decimalSeparator + "$", "");
+	if (!aux.contains("" + decimalSeparator)) {
+	    aux += decimalSeparator + "0";
 	}
-	if (aux.startsWith(".")) {
+	if (aux.startsWith("" + decimalSeparator)) {
 	    aux = "0" + aux;
 	}
 	return aux;
