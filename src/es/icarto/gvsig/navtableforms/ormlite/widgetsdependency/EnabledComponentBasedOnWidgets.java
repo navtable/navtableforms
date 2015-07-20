0d7d7fe8dcdd0ff4e8540cba41c80aa4c92023db
Francisco Puga <fpuga@icarto.es>
2015-07-20 08:28:38 +0200

    Allows the use of JDataChooser with ENABLEIF conditions

--- src/es/icarto/gvsig/navtableforms/ormlite/widgetsdependency/EnabledComponentBasedOnWidgets.java
+++ src/es/icarto/gvsig/navtableforms/ormlite/widgetsdependency/EnabledComponentBasedOnWidgets.java
@@ -1,8 +1,11 @@
 package es.icarto.gvsig.navtableforms.ormlite.widgetsdependency;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,22 +20,26 @@ import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import com.toedter.calendar.JDateChooser;
+
 import es.icarto.gvsig.navtableforms.IValidatableForm;
+import es.udc.cartolab.gvsig.navtable.format.DateFormatNT;
 
 public class EnabledComponentBasedOnWidgets implements ActionListener {
 
-    private JComponent component;
-    private Map<JComponent, List<String>> conditions = new HashMap<JComponent, List<String>>();
+    private final JComponent component;
+    private final Map<JComponent, List<String>> conditions = new HashMap<JComponent, List<String>>();
     private boolean removeDependentValues;
-    private IValidatableForm form;
+    private final IValidatableForm form;
     // to store the table listeners when the component is a jtable
     private MouseListener[] listeners = new MouseListener[0];
 
-    public EnabledComponentBasedOnWidgets(JComponent component, IValidatableForm form) {
+    public EnabledComponentBasedOnWidgets(JComponent component,
+	    IValidatableForm form) {
 	this.component = component;
 	this.form = form;
     }
-    
+
     public void addCondition(JComponent widget, String value) {
 	if (!conditions.containsKey(widget)) {
 	    conditions.put(widget, new ArrayList<String>());
@@ -77,11 +84,11 @@ public class EnabledComponentBasedOnWidgets implements ActionListener {
 	    if (widget instanceof JCheckBox) {
 		enable = enable
 			&& checkCheckBoxCondition((JCheckBox) widget,
-			conditions.get(widget));
+				conditions.get(widget));
 	    } else if (widget instanceof JComboBox) {
 		enable = enable
 			&& checkComboBoxCondition((JComboBox) widget,
-			conditions.get(widget));
+				conditions.get(widget));
 	    }
 	}
 	changeComponentState(enable, initialLoad);
@@ -89,7 +96,7 @@ public class EnabledComponentBasedOnWidgets implements ActionListener {
 
     private boolean checkComboBoxCondition(JComboBox widget, List<String> values) {
 	if (widget.getSelectedItem() != null) {
-	    String selected = ((JComboBox) widget).getSelectedItem().toString();
+	    String selected = widget.getSelectedItem().toString();
 	    for (String value : values) {
 		if (value.equalsIgnoreCase(selected)) {
 		    return true;
@@ -127,27 +134,40 @@ public class EnabledComponentBasedOnWidgets implements ActionListener {
 			component.removeMouseListener(l);
 		    }
 		}
-	    } else {
-		if ((component instanceof JComboBox) && !enabled) {
+	    } else if ((component instanceof JComboBox) && !enabled) {
 		    // If the component is a combobox and we are disabling
 		    // it, prior to that we select the default item.
 		    if (((JComboBox) component).getItemCount() > 0) {
 			((JComboBox) component).setSelectedIndex(0);
 		    }
-		} else {
-		    if ((component instanceof JCheckBox) && !enabled
+	    } else if ((component instanceof JCheckBox) && !enabled
 			    && ((JCheckBox) component).isSelected()) {
 			// If the component is a checkbox, we are disabling
 			// it and it was checked, prior to that we uncheck it.
 			((JCheckBox) component).doClick();
-		    }
-		}
-	    }
+	    } 
+		
+	    
 	    component.setEnabled(enabled);
+	    
+	    if ((component instanceof JDateChooser) && enabled) {
+		initDateChooser((JDateChooser) component);
+	    }
 	    if (removeDependentValues && !initialLoad) {
 		removeValue(component);
 	    }
 	}
+    }
+    
+    private void initDateChooser(JDateChooser c) {
+	SimpleDateFormat dateFormat = DateFormatNT.getDateFormat();
+	c.setDateFormatString(dateFormat.toPattern());
+	c.getDateEditor().setEnabled(false);
+	c.getDateEditor().getUiComponent()
+	.setBackground(new Color(255, 255, 255));
+	c.getDateEditor().getUiComponent()
+	.setFont(new Font("Arial", Font.PLAIN, 11));
+	c.getDateEditor().getUiComponent().setToolTipText(null);
     }
 
     private void removeValue(JComponent c) {
