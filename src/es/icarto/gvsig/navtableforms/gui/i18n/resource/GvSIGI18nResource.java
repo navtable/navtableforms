5096b5aad8d814d0cb93bc6ef68140389cc0144a
Jorge López Fernández <jlopez@cartolab.es>
2015-08-13 12:56:01 +0200

    Added an I18n resource for gvSIG i18n support

---
+++ src/es/icarto/gvsig/navtableforms/gui/i18n/resource/GvSIGI18nResource.java
@@ -0,0 +1,26 @@
+package es.icarto.gvsig.navtableforms.gui.i18n.resource;
+
+import org.gvsig.i18n.Messages;
+
+
+/**
+ * I18n resource based onto gvSIG's default i18n support (in the same way as PluginServices).
+ * 
+ * It will simply call org.gvsig.i18n.Messages.getText.
+ * 
+ * @author Jorge López Fernández <jlopez@cartolab.es>
+ *
+ */
+public class GvSIGI18nResource implements I18nResource {
+
+    @Override
+    public boolean containsKey(String key) {
+	return Messages.getText(key, false) != null;
+    }
+
+    @Override
+    public String getString(String key) {
+	return Messages.getText(key, false);
+    }
+
+}
