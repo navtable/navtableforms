8332c2226883a4ada2e48fcd0febb022215638c7
Jorge López Fernández <jlopez@cartolab.es>
2015-08-14 13:28:03 +0200

    Added method for retrieving i18n handler from forms.

--- src/es/icarto/gvsig/navtableforms/II18nForm.java
+++ src/es/icarto/gvsig/navtableforms/II18nForm.java
@@ -7,6 +7,8 @@ import es.icarto.gvsig.navtableforms.gui.i18n.resource.I18nResource;
 public interface II18nForm {
 
     public I18nResource[] getI18nResources();
+
+    public I18nHandler getI18nHandler();
     
     public FormPanel getFormPanel();
 
