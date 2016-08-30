2b915eaecc194ba2693f06958221b188900b50d4
Francisco Puga <fpuga@icarto.es>
2016-08-30 12:17:12 +0200

    Revert "Bug in Ab. parser which changed containers' names"
    This reverts commit 235917036c6e441cb21b855316c91f88c1b9e9f0.

--- src/main/java/es/icarto/gvsig/navtableforms/utils/AbeilleParser.java
+++ src/main/java/es/icarto/gvsig/navtableforms/utils/AbeilleParser.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2011. iCarto
- * 
+ *
  * This file is part of extNavTableForms
- * 
+ *
  * extNavTableForms is free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or any later version.
- * 
+ *
  * extNavTableForms is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with extNavTableForms.
  * If not, see <http://www.gnu.org/licenses/>.
  */
@@ -32,86 +32,89 @@ import javax.swing.JTextField;
 import com.toedter.calendar.JDateChooser;
 
 public class AbeilleParser {
-	
+
 	private static class NameTransformation {
 		public String transform(String org) {
 			String name = getNameBeforeDots(org);
 			return name.toLowerCase();
 		}
-		
+
 		/**
-	     * A legacy method to guarantee backwards compatibility. Should be sage
-	     * remove it in modern versions of navtableforms.
-	     */
-	    private String getNameBeforeDots(String widgetName) {
-		if (widgetName.contains(".")) {
-		    return widgetName.substring(0, widgetName.indexOf("."));
-		} else {
-		    return widgetName;
+		 * A legacy method to guarantee backwards compatibility. Should be sage
+		 * remove it in modern versions of navtableforms.
+		 */
+		private String getNameBeforeDots(String widgetName) {
+			if (widgetName.contains(".")) {
+				return widgetName.substring(0, widgetName.indexOf("."));
+			} else {
+				return widgetName;
+			}
 		}
-	    }
-		
+
 	}
 
 	private final static NameTransformation nameTransform = new NameTransformation();
-	
-    
 
-    /**
-     * This method used to return a map with the names of components uppercased,
-     * that was changed in named of performance and now is retrieved as it is.
-     */
-    public static HashMap<String, JComponent> getWidgetsFromContainer(
-	    Container c) {
-	HashMap<String, JComponent> map = new HashMap<String, JComponent>();
-	addContainerWidgets(c, map);
-	return map;
-    }
+	/**
+	 * This method used to return a map with the names of components uppercased,
+	 * that was changed in named of performance and now is retrieved as it is.
+	 */
+	public static HashMap<String, JComponent> getWidgetsFromContainer(
+			Container c) {
 
-    private static void addContainerWidgets(
-	    Container c, HashMap<String, JComponent> map) {
-	int count = 0;
-	while (c.getComponentCount() > count) {
-	    Component comp = c.getComponent(count++);
-	    if ((comp instanceof JTextField)
-		    || (comp instanceof JFormattedTextField)
-		    || (comp instanceof JCheckBox)
-		    || (comp instanceof JTextArea)
-		    || (comp instanceof JComboBox) || (comp instanceof JTable)
-		    || (comp instanceof JDateChooser)) {
-		String newName = nameTransform.transform(comp.getName());
-		comp.setName(newName);
-		map.put(comp.getName(), (JComponent) comp);
-	    }
+		HashMap<String, JComponent> map = new HashMap<String, JComponent>();
+		int count = 0;
+		while (c.getComponentCount() > count) {
+			Component comp = c.getComponent(count++);
+			if ((comp instanceof JTextField)
+					|| (comp instanceof JFormattedTextField)
+					|| (comp instanceof JCheckBox)
+					|| (comp instanceof JTextArea)
+					|| (comp instanceof JComboBox) || (comp instanceof JTable)
+					|| (comp instanceof JDateChooser)) {
+				String newName = nameTransform.transform(comp.getName());
+				comp.setName(newName);
+				map.put(comp.getName(), (JComponent) comp);
+				return map;
+			}
 
-	    if (comp instanceof Container) {
-		addContainerWidgets((Container) comp, map);
-	    }
-	}
-    }
-
-    public static HashMap<String, JButton> getButtonsFromContainer(Container c) {
-	HashMap<String, JButton> map = new HashMap<String, JButton>();
-	int count = 0;
-	while (c.getComponentCount() > count) {
-	    Component comp = c.getComponent(count++);
-	    if (comp.getClass().isAssignableFrom(JButton.class)) {
-	    	String newName = nameTransform.transform(comp.getName());
-		comp.setName(newName);
-		map.put(comp.getName(), (JButton) comp);
-		return map;
-	    } else if (comp instanceof Container) {
-		HashMap<String, JButton> recursiveMap = getButtonsFromContainer((Container) comp);
-		if (recursiveMap.size() > 0) {
-		    for (JButton w : recursiveMap.values()) {
-		    	String newName = nameTransform.transform(comp.getName());
-			comp.setName(newName);
-			map.put(w.getName(), w);
-		    }
+			if (comp instanceof Container) {
+				HashMap<String, JComponent> recursiveMap = getWidgetsFromContainer((Container) comp);
+				if (recursiveMap.size() > 0) {
+					for (JComponent w : recursiveMap.values()) {
+						String newName = nameTransform.transform(w.getName());
+						comp.setName(newName);
+						map.put(w.getName(), w);
+					}
+				}
+			}
 		}
-	    }
+		return map;
 	}
-	return map;
-    }
+
+	public static HashMap<String, JButton> getButtonsFromContainer(Container c) {
+		HashMap<String, JButton> map = new HashMap<String, JButton>();
+		int count = 0;
+		while (c.getComponentCount() > count) {
+			Component comp = c.getComponent(count++);
+			if (comp.getClass().isAssignableFrom(JButton.class)) {
+				String newName = nameTransform.transform(comp.getName());
+				comp.setName(newName);
+				map.put(comp.getName(), (JButton) comp);
+				return map;
+			} else if (comp instanceof Container) {
+				HashMap<String, JButton> recursiveMap = getButtonsFromContainer((Container) comp);
+				if (recursiveMap.size() > 0) {
+					for (JButton w : recursiveMap.values()) {
+						String newName = nameTransform
+								.transform(comp.getName());
+						comp.setName(newName);
+						map.put(w.getName(), w);
+					}
+				}
+			}
+		}
+		return map;
+	}
 
 }
